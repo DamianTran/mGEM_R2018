e1912b5a017231ff38eaab3ec19cdbe9ef812591
DamianTran <32999234+DamianTran@users.noreply.github.com>
2018-04-02 16:39:44 -0400

    Add files via upload

--- mGEM 2018/tutorial_func.R
+++ mGEM 2018/tutorial_func.R
@@ -1,14 +1,17 @@
 # Sets the working directory - change this to your desired project directory
 setwd("C:/Users/Damian/Documents/Tutorials/R/mGEM 2018/")
 
-NGSgen = function(ncol, nrow, seqLength){
-  randomMatrix = matrix(nrow = nrow, ncol = ncol)
-  for(y in 1:nrow){
-    for(x in 1:ncol){
-      randomMatrix[y,x] = randSeq(seqLength)
+NGSgen = function(ncol, nrow, seqLength = NA, seq = NA){
+  if(is.na(seq) & !is.na(seqLength)){
+    randomMatrix = matrix(nrow = nrow, ncol = ncol)
+    for(y in 1:nrow){
+      for(x in 1:ncol){
+        randomMatrix[y,x] = randSeq(seqLength)
+      }
     }
+    return(randomMatrix)
   }
-  return(randomMatrix)
+  return (matrix(seq, ncol, nrow))
 }
 
 randSeq = function(length){
@@ -22,4 +25,35 @@ randSeq = function(length){
     else randString[i] = "G"
   }
   return(paste(randString, collapse = ""))
+}
+
+randMutate = function(dataset, frequency){
+  colNum = ncol(dataset)
+  rowNum = nrow(dataset)
+  randNum = numeric(1)
+  mutThreshold = 1-frequency
+  for(y in 1:rowNum){
+    for(x in 1:colNum){
+      for(i in 1:nchar(dataset[y,x])){
+        randNum = runif(1)
+        if(randNum > mutThreshold){
+          randNum = runif(1)
+          if((randNum >= 0) & (randNum < 0.25)) substr(dataset[y,x], i, i) = "A"
+          else if((randNum >= 0.25) & (randNum < 0.5)) substr(dataset[y,x], i, i) = "T"
+          else if((randNum >= 0.5) & (randNum < 0.75)) substr(dataset[y,x], i, i) = "C"
+          else substr(dataset[y,x], i, i+1) = "G"
+        }
+      }
+    }
+  }
+  return(dataset)
+}
+
+aggregate = function(dataset){
+  uniqueSeq = unique(as.vector(dataset))
+  counts = numeric(length(uniqueSeq))
+  for(i in 1:length(uniqueSeq)){
+    counts[i] = sum(dataset == uniqueSeq[i]);
+  }
+  return(data.frame(uniqueSeq, counts))
 }
