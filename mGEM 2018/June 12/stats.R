8c8ea61230e0df7f9fe6a69034c5181e1e27512c
Damian Tran <trand22@mcmaster.ca>
2018-06-12 15:09:40 -0400

    Add files via upload

--- mGEM 2018/June 12/stats.R
+++ mGEM 2018/June 12/stats.R
@@ -27,9 +27,9 @@ z_score = (mutCount - avg)/st_err
 p_value = pnorm(z_score, lower.tail = FALSE) # Upper tail only - anything lower will return p = 1
 
 if(p_value <= 0.05){
-  cat("The sequence ", freq$uniqueSeq[1], " is signiicantly enriched in the population\n")
+  cat("The sequence ", freq$uniqueSeq[1], " is significantly enriched in the population\n")
 }else{
-  cat("The sequence ", freq$uniqueSeq[1], " is not signiicantly enriched in the population\n")
+  cat("The sequence ", freq$uniqueSeq[1], " is not significantly enriched in the population\n")
 }
 cat("Dataset avg: ", avg, "\nDataset SD: ", st_dev,
     "\nCount: ", mutCount, "\nTest score: ", z_score, 
@@ -39,9 +39,21 @@ mutCount = freq$counts[200] # A value lower on the sorted list will be unlikely 
 z_score = (mutCount - avg)/st_err
 p_value = pnorm(z_score, lower.tail = FALSE)
 if(p_value <= 0.05){
-  cat("The sequence ", freq$uniqueSeq[1], " is signiicantly enriched in the population\n")
+  cat("The sequence ", freq$uniqueSeq[1], " is significantly enriched in the population\n")
 }else{
-  cat("The sequence ", freq$uniqueSeq[1], " is not signiicantly enriched in the population\n")
+  cat("The sequence ", freq$uniqueSeq[1], " is not significantly enriched in the population\n")
+}
+cat("Dataset avg: ", avg, "\nDataset SD: ", st_dev,
+    "\nCount: ", mutCount, "\nTest score: ", z_score, 
+    "\nP value: ", p_value, '\n\n')
+
+mutCount = freq$counts[500] # A value lower on the sorted list will be unlikely to be significantly enriched
+z_score = (mutCount - avg)/st_err
+p_value = pnorm(z_score, lower.tail = FALSE)
+if(p_value <= 0.05){
+  cat("The sequence ", freq$uniqueSeq[1], " is significantly enriched in the population\n")
+}else{
+  cat("The sequence ", freq$uniqueSeq[1], " is not significantly enriched in the population\n")
 }
 cat("Dataset avg: ", avg, "\nDataset SD: ", st_dev,
     "\nCount: ", mutCount, "\nTest score: ", z_score, 
