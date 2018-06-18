3fb2dc0a8a16d07bf8ef3a0102dcd67e1341c950
Damian Tran <trand22@mcmaster.ca>
2018-06-18 14:27:06 -0500

    June 17 Tutorial - rBLAST and local alignment

--- mGEM 2018/June 17/NGSprocess.R
+++ mGEM 2018/June 17/NGSprocess.R
@@ -1,3 +1,8 @@
+if(!require("textreuse")){ # We will use the align_local() function here to check mutation positions
+  install.packages("textreuse")
+  library(textreuse)
+}
+
 # Aggregate the counts together in identical sequences
 aggregate = function(dataset, minThres = 0.7, maxThres = 1.3, minLength = 3){
   
@@ -22,4 +27,39 @@ aggregate = function(dataset, minThres = 0.7, maxThres = 1.3, minLength = 3){
   
   return(output[order(output$counts, decreasing = TRUE),])
 }
+
+# Function to port the align_local to DNA sequence alignment
+alignDNA = function(template, test){
+  
+  # template: the sequence to be compared against
+  # test: the sequence that we want to check the alignment of against the template
+  
+  # First we need to split the strings by spaces for the align_local function to work
+  template = paste(strsplit(template, split = "")[[1]], collapse = " ")
+  test = paste(strsplit(test, split = "")[[1]], collapse = " ")
+  
+  alignment_test = align_local(template, test)
+  
+  # Now we concatenate the string back together and check for edits indicated by the '#' character
+  alignment_test$a_edits = paste(strsplit(alignment_test$a_edits, split = " ")[[1]], collapse = "")
+  alignment_test$b_edits = paste(strsplit(alignment_test$b_edits, split = " ")[[1]], collapse = "")
+  
+  return(alignment_test)
+  
+}
+
+# Function for aggregation of mutants by mutation position
+aggregate_pos = function(counts, wild_type = NA){
+  if(nrow(counts) < 1) return(NA)
+  if(is.na(wild_type)) wild_type = counts$uniqueSeq[1]
+  
+  # TO DO: compare each sequence in the counts to the wild-type reference
+  # Tally the location of the edits, denoted by '#', and add an extra column
+  # to the counts dataframe containing the location of the edit
+  
+}
+
+# TO DO: write another function for the annotation of mutations by their type
+# ie. point mutation, silent mutation (will require a function that translates the DNA to amino acids)
+# or frame shift (indel)
 
